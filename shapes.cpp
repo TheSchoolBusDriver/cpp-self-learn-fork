79b214a2e4a64e83e497d7e06946a94ac5d735b6
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-16 03:19:55 +0500

    Code author & creation date set. Needs commenting

---
+++ shapes.cpp
@@ -0,0 +1,32 @@
+/*
+///////////////////////////////////////////////////
+// Name: Ahmed Affaan                            //
+// Title: shapes.cpp                             //
+// Date: 16/09/2021                              //
+// Country: Maldives                             //
+// Description: Program that prints out a shape  //
+//              using special characters.        //
+///////////////////////////////////////////////////
+*/
+
+// Program start
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "A Triangle\n--------------------" << endl;
+    cout << "       /|" << endl;
+    cout << "      / |" << endl;
+    cout << "     /  |" << endl;
+    cout << "    /   |" << endl;
+    cout << "   /    |" << endl;
+    cout << "  /     |" << endl;
+    cout << " /      |" << endl;
+    cout << "/_______|" << endl;
+    cout << "\n--------------------" << endl;
+    return 0;
+}
+
+// Program end
