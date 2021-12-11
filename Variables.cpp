af1f61da0bcf544ce656eae3700f21070b8d776c
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-11 06:43:22 +0500

    File created & description set

---
+++ Variables.cpp
@@ -0,0 +1,16 @@
+/*
+///////////////////////////////////////////////////
+// Name: Ahmed Affaan                            //
+// Title: Variables.cpp                          //
+// Date: 11/12/2021                              //
+// Country: Maldives                             //
+// Description: -                                //
+// Note:                                         //
+///////////////////////////////////////////////////
+*/
+
+// Program start.
+
+
+
+// Program end.
