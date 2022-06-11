848a6206885078a7e0b9a1856e6906c7cbbe5f9f
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-11 15:58:39 +0500

    Variables declared

--- Free_Code_Camp/variables.cpp
+++ Free_Code_Camp/variables.cpp
@@ -26,10 +26,10 @@ int main()
     characterAge = 23;
 
     // Will print out a message.
-    cout << "There was once a man named George" << endl;
-    cout << "He was 70 years old" << endl;
-    cout << "He liked the name George" << endl;
-    cout << "But did not like being 70" << endl;
+    cout << "There was once a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
 
     return 0;
 }
