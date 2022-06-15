5000d6ea5ab050207771033dd16454c32384bbdf
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-16 02:44:30 +0500

    Separator line added

--- Free_Code_Camp/numbers.cpp
+++ Free_Code_Camp/numbers.cpp
@@ -26,6 +26,7 @@ int main()
 
     // Printing a negative number.
     cout << -40 << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
 
     // Printing a negative decimal number.
     cout << -40.09823 << endl;
