26a311da4322eec71e33569bf5497241f88b225b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-16 03:17:17 +0500

    File description set

---
+++ Free_Code_Camp/user_input.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Ahmed Affaan
+Title: user_input.cpp
+Folder: CPP/Free_Code_Camp
+Date: 16/06/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=vLnPwxZdW4Y
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+
+
+// Program end.
