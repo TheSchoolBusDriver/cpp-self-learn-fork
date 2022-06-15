615bc29f55f338d77625867bfeaca69d4268c2fd
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-16 02:41:00 +0500

    Will decrement from variable

--- Free_Code_Camp/numbers.cpp
+++ Free_Code_Camp/numbers.cpp
@@ -65,6 +65,9 @@ int main()
     // Increments certain amount to variable.
     wnum += 80;
 
+    // Decrements certain amount from variable.
+    wnum -= 80;
+
     // Will give a decimal number as answer.
     cout << 5.5 + 9 << endl;
 
