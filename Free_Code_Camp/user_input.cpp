37eafae26d0400541ad29820a8a287b3bc0069e3
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-17 04:08:25 +0500

    Code uncommented

--- Free_Code_Camp/user_input.cpp
+++ Free_Code_Camp/user_input.cpp
@@ -32,7 +32,6 @@ int main()
     cout << "Your age is " << age << " years old." << endl;
     */
 
-    /*
     // Variable to store users name.
     string name;
 
@@ -42,7 +41,6 @@ int main()
 
     // Printing the output.
     cout << "Well hello " << name << "." << endl;
-    */
 
     return 0;
 }
