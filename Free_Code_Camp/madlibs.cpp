0ad3a86b49428e7a8984f771ff7df7fc9a162ad3
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-18 02:56:35 +0500

    File description set

---
+++ Free_Code_Camp/madlibs.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Ahmed Affaan
+Title: madlibs.cpp
+Folder: CPP/Free_Code_Camp
+Date: 18/06/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=vLnPwxZdW4Y
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+
+
+// Program end.
