b9c0295803c9d04002baf9477bb95a51bcff81f6
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-24 18:50:52 +0500

    Function will now calculate day name

--- Free_Code_Camp/switch_statements.cpp
+++ Free_Code_Camp/switch_statements.cpp
@@ -18,6 +18,52 @@ Note: Uncomment codes to execute and comment them when not in use.
 
 using namespace std;
 
+// This function will convert a number in to the day of the week.
+string getDayOfWeek(int dayNum)
+{
+    // Variables to store the name of the day.
+    string dayName;
+
+    // Will check the day numbers and give back day names.
+    switch(dayNum)
+    {
+        // Sunday.
+        case 0:
+        dayName = "Sunday";
+        break;
+        // Monday.
+        case 1:
+        dayName = "Monday";
+        break;
+        // Tuesday.
+        case 2:
+        dayName = "Tuesday";
+        break;
+        // Wednesday.
+        case 3:
+        dayName = "Wednesday";
+        break;
+        // Thursday.
+        case 4:
+        dayName = "Thursday";
+        break;
+        // Friday.
+        case 5:
+        dayName = "Friday";
+        break;
+        // Saturday.
+        case 6:
+        dayName = "Saturday";
+        break;
+        // Invalid option.
+        default:
+        dayName = "INVALID DAY NUMBER!";
+    }
+
+    // Returns back the day's name.
+    return dayName;
+}
+
 // Main function.
 int main()
 {
