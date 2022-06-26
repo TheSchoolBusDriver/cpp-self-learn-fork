18c0d8fa2eeec458c39926cb592fcf130fa366e3
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-26 14:59:36 +0500

    File description updated

--- Free_Code_Camp/guessing_game.cpp
+++ Free_Code_Camp/guessing_game.cpp
@@ -5,7 +5,7 @@ Folder: CPP/Free_Code_Camp
 Date: 26/06/2022
 Country: Republic of Maldives
 Code version: -
-Description: -
+Description: User guesses secret number.
 Credits to: Giraffe Academy
 Credits to link: https://www.youtube.com/watch?v=vLnPwxZdW4Y
 Note: Uncomment codes to execute and comment them when not in use.
