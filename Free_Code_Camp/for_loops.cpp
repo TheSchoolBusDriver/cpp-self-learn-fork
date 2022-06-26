5acf69e006e2bf06c8ddb94a88dc0211e18bf39d
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-26 15:35:41 +0500

    File description and template code set

---
+++ Free_Code_Camp/for_loops.cpp
@@ -0,0 +1,29 @@
+/*
+Name: Ahmed Affaan
+Title: for_loops.cpp
+Folder: CPP/Free_Code_Camp
+Date: 26/06/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=vLnPwxZdW4Y
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing libraries.
+#include <iostream>
+
+using namespace std;
+
+// Main function.
+int main()
+{
+    // Printing a message.
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+// Program end.
