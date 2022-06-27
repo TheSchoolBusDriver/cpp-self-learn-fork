b381f2b4b64cf5cbf87fb339137ff08cda0117fc
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-27 08:44:42 +0500

    Code commented out

--- Free_Code_Camp/for_loops.cpp
+++ Free_Code_Camp/for_loops.cpp
@@ -33,11 +33,13 @@ int main()
     }
     */
 
+    /*
     // Prints out series of numbers 1-5 (FOR Loop).
     for(int i = 1; i <= 5; i++)
     {
-        cout << i << endl; 
+        cout << i << endl;
     }
+    */
 
     return 0;
 }
