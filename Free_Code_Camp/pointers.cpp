725c6f29d55af11fd9877fa2b24576f161e1407b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-30 10:30:51 +0500

    Variables declared

--- Free_Code_Camp/pointers.cpp
+++ Free_Code_Camp/pointers.cpp
@@ -21,7 +21,11 @@ using namespace std;
 // Main function.
 int main()
 {
-    
+    // Variables to store age, gpa and name.
+    int age = 19;
+    double gpa = 2.7;
+    string name = "Affaan";
+
     return 0;
 }
 
