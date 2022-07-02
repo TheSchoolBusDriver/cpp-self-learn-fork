700474b0f746abc79a459c00876e094f5025d155
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-02 12:12:21 +0500

    Object attributes removed

--- Free_Code_Camp/constructor_functions.cpp
+++ Free_Code_Camp/constructor_functions.cpp
@@ -41,9 +41,6 @@ int main()
 {
     // Creating a book object (Object 01).
     Book book1;
-    book1.title = "Harry Potter";
-    book1.author = "J.K Rowling";
-    book1.pages = 600;
 
     // Creating a book object (Object 02).
     Book book2;
