a4a113100b89f099ccc5f8e8194c6327ed955af8
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-04 16:41:55 +0500

    Displays Movie 01 rating

--- Free_Code_Camp/getters_and_setters.cpp
+++ Free_Code_Camp/getters_and_setters.cpp
@@ -42,6 +42,9 @@ int main()
     // Creating a Movie object (Movie 01).
     Movie avengers("The Avengers", "Joss Whedon", "PG-13");
 
+    // Accessing Movie object attributes (Movie 01).
+    cout << "Rating: " << avengers.rating << endl;
+
     return 0;
 }
 
