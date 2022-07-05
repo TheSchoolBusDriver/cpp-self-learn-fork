147058faf146950112fdb28b94b0f65e079b577d
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-05 18:43:56 +0500

    Code commented out

--- Free_Code_Camp/getters_and_setters.cpp
+++ Free_Code_Camp/getters_and_setters.cpp
@@ -62,8 +62,10 @@ int main()
     // Creating a Movie object (Movie 01).
     Movie avengers("The Avengers", "Joss Whedon", "PG-13");
 
+    /*
     // Accessing Movie object attributes (Movie 01).
     cout << "Rating: " << avengers.rating << endl;
+    */
 
     return 0;
 }
