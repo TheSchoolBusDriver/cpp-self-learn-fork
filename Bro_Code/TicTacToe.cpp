6e6a6755c43c7ad7fd6eb906e7fbaab947e4cd60
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-08-07 21:26:01 +0500

    New file created

---
+++ Bro_Code/TicTacToe.cpp
@@ -0,0 +1,29 @@
+/*
+Name: Ahmed Affaan
+Title: TicTacToe.cpp
+Folder: CPP/Free_Code_Camp
+Date: 07/08/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=vLnPwxZdW4Y
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing header files.
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+// Main function.
+int main(){
+    // Printing a message.
+    cout << "Hello World!\n" << endl;
+    return 0;
+}
+
+// Program end.
