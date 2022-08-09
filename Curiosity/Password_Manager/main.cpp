d5b63bb70db65d1207ef8e7e1ee49eab27b4025a
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-08-10 00:40:12 +0500

    New file created

---
+++ Curiosity/Password_Manager/main.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Ahmed Affaan
+Title: main.cpp
+Folder: CPP/Curiosity/Password_Manager
+Date: 10/08/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing header files.
+#include <iostream>
+
+// Main function.
+int main(){
+    // Printing a message.
+    std::cout << "Hello World!" << '\n';
+    return 0;
+}
+
+// Program end.
