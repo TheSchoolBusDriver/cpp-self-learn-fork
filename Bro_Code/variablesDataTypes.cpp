6eb6e4ba1a8408d4479f91d0d48305662e82d9b7
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-08-11 22:04:55 +0500

    New file created

---
+++ Bro_Code/variablesDataTypes.cpp
@@ -0,0 +1,26 @@
+/*
+Name: Ahmed Affaan
+Title: variablesDataTypes.c
+Folder: CPP/Bro_Code
+Date: 11/08/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: BroCode
+Credits to link: https://www.youtube.com/watch?v=-TkoO8Z07hI
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing header files.
+#include <iostream>
+
+// Main function.
+int main(){
+    // Printing a message.
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+// Program end.
